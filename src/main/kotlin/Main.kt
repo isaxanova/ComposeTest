c102c6242aa2ace454e1a0c0200969443700cb3a
alexander.topilskii <alexander.topilskii@gmail.com>
2022-04-27 23:14:38 +0300

    добавление слайдеров для цвета

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,9 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -28,38 +25,32 @@ fun App() {
     Row {
         displayCalculationColumn()
 
-        var textFieldContent: String by rememberSaveable { mutableStateOf("") }
-        var buttonText: String by rememberSaveable { mutableStateOf("отправить") }
-        Column {
-            TextField(
-                value = textFieldContent,
-                onValueChange = { textFieldContent = it }
-            )
-            Button(
-                modifier = Modifier.padding(start = 15.dp),
-                content = { Text(buttonText) },
-                onClick = { buttonText = "отправлено" }
-            )
-            Text(text = textFieldContent)
-
-            var sliderPosition by remember { mutableStateOf(0f) }
-            var sliderColors by remember { mutableStateOf(0f) }
+        Column (Modifier.width(700.dp)) {
+            var sliderColorsR by remember { mutableStateOf(0f) }
+            var sliderColorsG by remember { mutableStateOf(0f) }
+            var sliderColorsB by remember { mutableStateOf(0f) }
             Box(
-                Modifier
-                    .width(sliderPosition.dp)
+                modifier = Modifier
+                    .width(700.dp)
                     .height(200.dp)
-                    .background(color = Color(sliderColors, sliderColors, sliderColors))
+                    .background(color = Color(red = sliderColorsR, green = sliderColorsG, blue = sliderColorsB))
             )
-            Text(text = "Текущее значение: $sliderPosition", fontSize = 22.sp)
+            Text(text = "Текущее значение: $sliderColorsB", fontSize = 22.sp)
             Slider(
-                value = sliderPosition,
-                onValueChange = { sliderPosition = it },
-                valueRange = 0f..200f
+                value = sliderColorsB,
+                onValueChange = { sliderColorsB = it },
+                valueRange = 0f..1f
             )
-            Text(text = "Текущее значение: $sliderColors", fontSize = 22.sp)
+            Text(text = "Текущее значение: $sliderColorsR", fontSize = 22.sp)
             Slider(
-                value = sliderColors,
-                onValueChange = { sliderColors = it },
+                value = sliderColorsR,
+                onValueChange = { sliderColorsR = it },
+                valueRange = 0f..1f
+            )
+            Text(text = "Текущее значение: $sliderColorsG", fontSize = 22.sp)
+            Slider(
+                value = sliderColorsG,
+                onValueChange = { sliderColorsG = it },
                 valueRange = 0f..1f
             )
         }
