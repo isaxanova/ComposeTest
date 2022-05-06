196222f39305df7b6ac7aca3cd3cf41acc72fdd3
alexander.topilskii <alexander.topilskii@gmail.com>
2022-05-06 18:56:36 +0300

    сделали заготовку для работ с функцией

---
+++ src/main/kotlin/workWithText.kt
@@ -0,0 +1,53 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        displayText()
+    }
+}
+
+@Composable
+@Preview
+fun displayText() {
+    var inputText by rememberSaveable { mutableStateOf("") }
+    var resultText by rememberSaveable { mutableStateOf("") }
+    Row {
+        Column(modifier = Modifier.padding(horizontal = 15.dp)) {
+            TextField(
+                value = inputText,
+                onValueChange = {
+                    inputText = it
+                }
+            )
+        }
+        Column(modifier = Modifier.padding(horizontal = 15.dp)) {
+            Button(
+                content = { Text("ПОСЧИТАТЬ") },
+                onClick = {
+                    resultText = inputText
+                }
+            )
+        }
+        Column(modifier = Modifier.padding(horizontal = 15.dp)) {
+            Text(resultText)
+        }
+    }
+}
